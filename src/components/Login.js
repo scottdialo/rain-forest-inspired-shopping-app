173b6b43461e7eb4197e3dad812f1a0c40f75be5
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-12-04 09:00:55 -0800

    styled login icon

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -29,10 +29,23 @@ export default function Login() {
 
   return (
     <div>
+      <div class="jss1 MuiAvatar-root MuiAvatar-circle jss2 " style={{fill: "white"}}>
+        <svg
+          class="MuiSvgIcon-root"
+          focusable="false"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+        >
+          <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
+        </svg>
+      
+      </div>
+
       <h4 className="login-title">Login</h4>
       <div>
         <label>Email</label>
-        <input autoFocus
+        <input
+          autoFocus
           type="text"
           onChange={(e) => {
             setLoginEmail(e.target.value);
@@ -49,6 +62,9 @@ export default function Login() {
       <button onClick={login} className="login-btn">
         Login
       </button>
+      <Link to="/registration">
+        <p className="register-text">Don't have an account? Sign Up</p>
+      </Link>
     </div>
   );
 }
