410add637f57d30a94832637d011d29e0d129592
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-12-04 13:22:27 -0800

    made some changes to login file

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,15 +21,18 @@ export default function Login() {
           state: "Hello World!",
         });
       })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+      .catch((err) => {
+        const errorCode = err.code;
+        const errorMessage = err.message;
       });
   };
 
   return (
     <div>
-      <div class="jss1 MuiAvatar-root MuiAvatar-circle jss2 " style={{fill: "white"}}>
+      <div
+        class="jss1 MuiAvatar-root MuiAvatar-circle jss2 "
+        style={{ fill: "white" }}
+      >
         <svg
           class="MuiSvgIcon-root"
           focusable="false"
@@ -38,7 +41,6 @@ export default function Login() {
         >
           <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
         </svg>
-      
       </div>
 
       <h4 className="login-title">Login</h4>
